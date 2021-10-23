a6ac0f2113685d7c9a2b9353c2d37a9c181ad728
Aytixel <aytixel@gmail.com>
2021-10-24 01:50:50 +0200

    Update app.js

--- src/runnable/app.js
+++ src/runnable/app.js
@@ -51,160 +51,164 @@ export default class {
       "args": ["--no-sandbox", "--disable-dev-shm-usage"],
     });
     const page = (await browser.pages())[0];
-    const getData = () => {
-      setTimeout(async () => {
-        const waitToClick = async (selector) => {
-          await page.waitForSelector(selector, { visible: true });
-          await page.evaluate((selector) => {
-            document.querySelector(selector).click();
-          }, selector);
-        };
+    const getData = async () => {
+      const waitToClick = async (selector) => {
+        while (1) {
+          try {
+            await page.waitForSelector(selector, { visible: true });
+            await page.waitForTimeout(1000);
+            await page.click(selector);
 
-        try {
-          await waitToClick(
-            "#x-auto-35 > tbody > tr:nth-child(2) > td.x-btn-mc > em > button",
+            break;
+          } catch (error) {}
+        }
+      };
+
+      try {
+        await page.waitForTimeout(10000);
+        await waitToClick(
+          "#x-auto-35 > tbody > tr:nth-child(2) > td.x-btn-mc > em > button",
+        );
+        await waitToClick(
+          "div[role='alertdialog'] tbody > tr:nth-child(2) > td.x-btn-mc > em > button",
+        );
+
+        const page2 = await (await browser.waitForTarget(
+          (target) =>
+            target.url().startsWith(
+              "http://planning.univ-lemans.fr/direct/gwtdirectplanning/rss",
+            ),
+        )).page();
+
+        if (page2) {
+          const clientId = new URL(page2.url()).searchParams.get(
+            "cliendId",
           );
-          await waitToClick(
-            "div[role='alertdialog'] tbody > tr:nth-child(2) > td.x-btn-mc > em > button",
-          );
 
-          const page2 = await (await browser.waitForTarget(
-            (target) =>
-              target.url().startsWith(
-                "http://planning.univ-lemans.fr/direct/gwtdirectplanning/rss",
-              ),
-          )).page();
+          page2.close();
 
-          if (page2) {
-            const clientId = new URL(page2.url()).searchParams.get(
-              "cliendId",
-            );
+          const planningData = await page.evaluate(
+            async (clientId, resourcesId) => {
+              const promises = [];
 
-            page2.close();
-
-            const planningData = await page.evaluate(
-              async (clientId, resourcesId) => {
-                const promises = [];
-
-                for (const key in resourcesId) {
-                  resourcesId[key].forEach((resourceId, index) => {
-                    promises.push(
-                      fetch(
-                        "http://planning.univ-lemans.fr/direct/gwtdirectplanning/rss?projectId=1&resources=" +
-                          resourceId + "&cliendId=" + clientId +
-                          "&nbDays=15&since=0",
-                      ).then((response) => response.text()).then((data) =>
-                        new Promise((resolve) => {
-                          resourcesId[key][index] = data;
-                          resolve();
-                        })
-                      ),
-                    );
-                  });
-                }
-
-                await Promise.all(promises);
-
-                return resourcesId;
-              },
-              clientId,
-              this.resourcesId,
-            );
-
-            for (const key in planningData) {
-              planningData[key].forEach((data, index) => {
-                const newCourcesData = {};
-                const addZero = (number) => {
-                  if (number < 10) return "0" + number;
-
-                  return number.toString();
-                };
-
-                for (let i = 0; i < 16; i++) {
-                  const date = new Date(
-                    Date.now() + 1000 * 3600 * 24 * i,
+              for (const key in resourcesId) {
+                resourcesId[key].forEach((resourceId, index) => {
+                  promises.push(
+                    fetch(
+                      "http://planning.univ-lemans.fr/direct/gwtdirectplanning/rss?projectId=1&resources=" +
+                        resourceId + "&cliendId=" + clientId +
+                        "&nbDays=15&since=0",
+                    ).then((response) => response.text()).then((data) =>
+                      new Promise((resolve) => {
+                        resourcesId[key][index] = data;
+                        resolve();
+                      })
+                    ),
                   );
+                });
+              }
 
-                  newCourcesData[
-                    `${date.getUTCFullYear()}/${
-                      addZero(
-                        date.getUTCMonth() +
-                          1,
-                      )
-                    }/${addZero(date.getUTCDate())}`
-                  ] = [];
-                }
+              await Promise.all(promises);
 
-                // parse and pre-format data
-                parse(data).rss.channel.item.forEach(
-                  (courseData) => {
-                    const parsedCourseData = courseData.description.replace(
-                      /<.>/g,
-                      "",
-                    )
-                      .replace(
-                        /<\/.>/g,
-                        "<br/>",
-                      ).replace(/<br \/>/g, "<br/>").split("<br/>").filter((
-                        x,
-                        index,
-                        array,
-                      ) => x && index != array.length - 1);
-                    const parsedDate = parsedCourseData[0].match(
-                      /\d\d\/\d\d\/\d{4,6}|\d\d:\d\d/g,
-                    );
-                    const dateString = parsedDate[0]
-                      .split(
-                        "/",
-                      ).reverse().join("/");
-                    const startOfComment = parsedCourseData.lastIndexOf(
-                      "Comment",
-                    );
+              return resourcesId;
+            },
+            clientId,
+            this.resourcesId,
+          );
 
-                    newCourcesData[dateString].push({
-                      title: courseData.title.replace(/ +/g, " "),
-                      startDate: new Date(
-                        `${dateString} ${parsedDate[1]} GMT+00:00`,
-                      ),
-                      endDate: new Date(
-                        `${dateString} ${parsedDate[2]} GMT+00:00`,
-                      ),
-                      resources: parsedCourseData.slice(
-                        2,
-                        startOfComment,
-                      ),
-                      comment: parsedCourseData.slice(
-                        startOfComment + 1,
-                      ),
-                    });
-                  },
+          for (const key in planningData) {
+            planningData[key].forEach((data, index) => {
+              const newCourcesData = {};
+              const addZero = (number) => {
+                if (number < 10) return "0" + number;
+
+                return number.toString();
+              };
+
+              for (let i = 0; i < 16; i++) {
+                const date = new Date(
+                  Date.now() + 1000 * 3600 * 24 * i,
                 );
 
-                // final data formatting
-                for (const dateKeyString in newCourcesData) {
-                  const dateKey = new Date(
-                    `${dateKeyString} GMT+00:00`,
+                newCourcesData[
+                  `${date.getUTCFullYear()}/${
+                    addZero(
+                      date.getUTCMonth() +
+                        1,
+                    )
+                  }/${addZero(date.getUTCDate())}`
+                ] = [];
+              }
+
+              // parse and pre-format data
+              parse(data).rss.channel.item.forEach(
+                (courseData) => {
+                  const parsedCourseData = courseData.description.replace(
+                    /<.>/g,
+                    "",
+                  )
+                    .replace(
+                      /<\/.>/g,
+                      "<br/>",
+                    ).replace(/<br \/>/g, "<br/>").split("<br/>").filter((
+                      x,
+                      index,
+                      array,
+                    ) => x && index != array.length - 1);
+                  const parsedDate = parsedCourseData[0].match(
+                    /\d\d\/\d\d\/\d{4,6}|\d\d:\d\d/g,
+                  );
+                  const dateString = parsedDate[0]
+                    .split(
+                      "/",
+                    ).reverse().join("/");
+                  const startOfComment = parsedCourseData.lastIndexOf(
+                    "Comment",
                   );
 
-                  this.planningDB.db.collection(key).updateOne(
-                    { date: dateKey, group: index },
-                    {
-                      $set: {
-                        date: dateKey,
-                        group: index,
-                        cources: newCourcesData[dateKeyString],
-                      },
+                  newCourcesData[dateString].push({
+                    title: courseData.title.replace(/ +/g, " "),
+                    startDate: new Date(
+                      `${dateString} ${parsedDate[1]} GMT+00:00`,
+                    ),
+                    endDate: new Date(
+                      `${dateString} ${parsedDate[2]} GMT+00:00`,
+                    ),
+                    resources: parsedCourseData.slice(
+                      2,
+                      startOfComment,
+                    ),
+                    comment: parsedCourseData.slice(
+                      startOfComment + 1,
+                    ),
+                  });
+                },
+              );
+
+              // final data formatting
+              for (const dateKeyString in newCourcesData) {
+                const dateKey = new Date(
+                  `${dateKeyString} GMT+00:00`,
+                );
+
+                this.planningDB.db.collection(key).updateOne(
+                  { date: dateKey, group: index },
+                  {
+                    $set: {
+                      date: dateKey,
+                      group: index,
+                      cources: newCourcesData[dateKeyString],
                     },
-                    { upsert: true },
-                  );
-                }
-              });
-            }
+                  },
+                  { upsert: true },
+                );
+              }
+            });
           }
-        } catch (error) {
-          console.error(error);
         }
-      }, 1000 * 10);
+      } catch (error) {
+        console.error(error);
+      }
     };
     const update = async () => {
       await page.goto("http://planning.univ-lemans.fr/direct/myplanning.jsp");
@@ -213,8 +217,9 @@ export default class {
         await page.type("#username", this.studentUsername);
         await page.type("#password", this.studentPassword);
         await page.click("#fm1 > div:nth-child(3) > div > div > button");
+
         page.on("load", getData);
-      } else getData();
+      } else getData().catch(console.error);
     };
 
     update();
