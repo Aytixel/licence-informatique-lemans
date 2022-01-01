9be31a96708cf7b190de8a902d938bdb8f96ec10
Aytixel <aytixel@gmail.com>
2022-01-01 21:59:57 +0100

    Fix with the language correction commit

--- src/public/www/js/index.js
+++ src/public/www/js/index.js
@@ -1,27 +1,27 @@
-// cource viewer
-const cource_viewer_element = document.querySelector(
-  ".planning .cource-viewer",
+// course viewer
+const course_viewer_element = document.querySelector(
+  ".planning .course-viewer",
 );
-let is_cource_viewer_visible = false;
-const set_cource_viewer_visibility = (visible) => {
-  requestAnimationFrame(() => is_cource_viewer_visible = visible);
+let is_course_viewer_visible = false;
+const set_course_viewer_visibility = (visible) => {
+  requestAnimationFrame(() => is_course_viewer_visible = visible);
 
-  cource_viewer_element.setAttribute("aria-hidden", !visible);
+  course_viewer_element.setAttribute("aria-hidden", !visible);
 };
 
-document.querySelectorAll(".planning .cource").forEach((cource_element) => {
+document.querySelectorAll(".planning .course").forEach((course_element) => {
   const display = () => {
-    set_cource_viewer_visibility(true);
+    set_course_viewer_visibility(true);
 
-    cource_viewer_element.querySelector("h2").textContent =
-      cource_element.querySelector("h2").textContent;
-    cource_viewer_element.querySelector(".resources").innerHTML = cource_element
+    course_viewer_element.querySelector("h2").textContent =
+      course_element.querySelector("h2").textContent;
+    course_viewer_element.querySelector(".resources").innerHTML = course_element
       .dataset.resources.replaceAll(",", "<br/>");
-    cource_viewer_element.querySelector(".comment").innerHTML = cource_element
+    course_viewer_element.querySelector(".comment").innerHTML = course_element
       .dataset.comment.replaceAll(",", "<br/>");
   };
 
-  cource_element.addEventListener("click", (e) => {
+  course_element.addEventListener("click", (e) => {
     e.preventDefault();
 
     display();
@@ -31,42 +31,42 @@ document.querySelectorAll(".planning .cource").forEach((cource_element) => {
     if (e.key == "Enter") {
       e.preventDefault();
 
-      setTimeout(() => cource_viewer_element.focus(), 200);
+      setTimeout(() => course_viewer_element.focus(), 200);
 
       display();
     }
   };
 
-  cource_element.addEventListener("focus", () => {
-    cource_element.addEventListener("keydown", on_keydown);
+  course_element.addEventListener("focus", () => {
+    course_element.addEventListener("keydown", on_keydown);
   });
-  cource_element.addEventListener("blur", () => {
-    cource_element.removeEventListener("keydown", on_keydown);
+  course_element.addEventListener("blur", () => {
+    course_element.removeEventListener("keydown", on_keydown);
   });
 });
 
 document.querySelector(
-  ".planning .cource-viewer .close",
+  ".planning .course-viewer .close",
 ).addEventListener("click", () => {
-  set_cource_viewer_visibility(false);
+  set_course_viewer_visibility(false);
 });
 
 window.addEventListener("keydown", (e) => {
   if (e.key == "Escape") {
     e.preventDefault();
 
-    set_cource_viewer_visibility(false);
+    set_course_viewer_visibility(false);
   }
 });
 
 window.addEventListener("click", (e) => {
   if (
-    !(e.target.parentNode == cource_viewer_element ||
-      e.target == cource_viewer_element ||
-      e.target.parentNode?.classList?.contains("cource") ||
-      e.target.classList?.contains("cource")) &&
-    is_cource_viewer_visible
+    !(e.target.parentNode == course_viewer_element ||
+      e.target == course_viewer_element ||
+      e.target.parentNode?.classList?.contains("course") ||
+      e.target.classList?.contains("course")) &&
+    is_course_viewer_visible
   ) {
-    set_cource_viewer_visibility(false);
+    set_course_viewer_visibility(false);
   }
 });
