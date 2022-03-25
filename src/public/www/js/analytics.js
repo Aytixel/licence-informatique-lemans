84eab445f89db28619127f4d6d87e465a876dde6
Aytixel <aytixel@gmail.com>
2022-03-25 21:01:05 +0100

    Add Google wevVitals to analytics

--- src/public/www/js/analytics.js
+++ src/public/www/js/analytics.js
@@ -1,20 +1,41 @@
 const load_analytics = () => {
   const tag_manager_script = document.createElement("script");
-  const analytics_script = document.createElement("script");
+  const web_vitals_script = document.createElement("script");
 
   tag_manager_script.src =
     "https://www.googletagmanager.com/gtag/js?id=G-B5EYVWF0Z9";
   tag_manager_script.setAttribute("async", "");
-  analytics_script.textContent = `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+  tag_manager_script.onload = () => {
+    window.dataLayer = window.dataLayer || [];
 
-        gtag('config', 'G-B5EYVWF0Z9');
-    `;
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+
+    gtag("js", new Date());
+    gtag("config", "G-B5EYVWF0Z9");
+
+    web_vitals_script.src =
+      "https://unpkg.com/web-vitals/dist/web-vitals.iife.js";
+    web_vitals_script.setAttribute("async", "");
+    web_vitals_script.onload = function () {
+      function sendToGoogleAnalytics({ name, delta, value, id }) {
+        gtag("event", name, {
+          value: delta,
+          metric_id: id,
+          metric_value: value,
+          metric_delta: delta,
+        });
+      }
+
+      webVitals.getCLS(sendToGoogleAnalytics);
+      webVitals.getFID(sendToGoogleAnalytics);
+      webVitals.getLCP(sendToGoogleAnalytics);
+    };
+  };
 
   document.head.appendChild(tag_manager_script);
-  document.head.appendChild(analytics_script);
+  document.head.appendChild(web_vitals_script);
 };
 
 if (+Cookies.get("gg_analytics")) load_analytics();
