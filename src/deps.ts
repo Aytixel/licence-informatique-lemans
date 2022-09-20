7cdeacb935614062f70727d7f02be0e054eff17e
Aytixel <aytixel@gmail.com>
2022-09-20 16:27:02 +0200

    Update deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
 // third party modules
-export { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-export type { DotenvConfig } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+export { config } from "https://deno.land/std@0.156.0/dotenv/mod.ts";
+export type { DotenvConfig } from "https://deno.land/std@0.156.0/dotenv/mod.ts";
 export { compress as brotli_compress } from "https://deno.land/x/brotli@v0.1.4/mod.ts";
 export { deflate, gzip } from "https://deno.land/x/compress@v0.4.5/mod.ts";
 
@@ -12,6 +12,6 @@ export {
   join,
   parse,
   resolve,
-} from "https://deno.land/std@0.155.0/path/mod.ts";
-export type { ParsedPath } from "https://deno.land/std@0.155.0/path/mod.ts";
-export { readRange } from "https://deno.land/std@0.155.0/io/files.ts";
+} from "https://deno.land/std@0.156.0/path/mod.ts";
+export type { ParsedPath } from "https://deno.land/std@0.156.0/path/mod.ts";
+export { readRange } from "https://deno.land/std@0.156.0/io/files.ts";
